76b4f65a5a3ca5381d2a6df1c8f6398f68324509
WONSEOK <wonseok0714@gmail.com>
2024-07-07 23:00:01 +0900

    240707

---
+++ SRC/Permutation_Combination/07_Permutation_repeatation_sampling.cpp
@@ -0,0 +1,69 @@
+/*
+https://www.acmicpc.net/problem/15656
+중복을 허용하는 임의의 데이터 M개 선택
+
+N개의 자연수와 자연수 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오. N개의 자연수는 모두 다른 수이다.
+
+N개의 자연수 중에서 M개를 고른 수열
+같은 수를 여러 번 골라도 된다.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+const int MAX_N = 8 + 2;
+int N, M, ans[MAX_N], trr[MAX_N], arr[MAX_N];
+
+void mergeSort(int start, int end){
+    //0. Base Condition
+    if(start >= end) return;
+
+    //1. Divided
+    int mid = (start + end) / 2;
+    
+    //2. conquer
+    mergeSort(start, mid);
+    mergeSort(mid + 1, end);
+
+    //3. merge
+    int i = start, j = mid + 1, k = start;
+    while( i <= mid && j <= end){
+        if(arr[i] < arr[j])
+            trr[k++] = arr[i++];
+        else 
+            trr[k++] = arr[j++];
+    }
+    while( i <= mid ) trr[k++] = arr[i++];
+    while( j <= end ) trr[k++] = arr[j++];
+
+    //4. Copy
+    for(int i = start; i <= end; i++)
+        arr[i] = trr[i];
+}
+
+void DFS(int depth){
+    if(depth == M){
+        for(int i = 0 ; i < M; i++)
+            printf("%d ", ans[i]);
+        printf("\n");
+        return;
+    }
+    for(int i = 0 ; i < N; i++){
+        ans[depth] = arr[i];
+        DFS(depth + 1);
+        ans[depth] = 0;
+    }
+}
+
+int main(){
+
+    scanf("%d %d", &N, &M);
+    for(int i = 0 ; i < N; i++)
+        scanf("%d", &arr[i]);
+    mergeSort(0,N-1);
+    DFS(0);
+
+    return 0;
+
+}
